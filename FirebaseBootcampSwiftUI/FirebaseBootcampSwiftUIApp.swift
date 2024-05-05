a2bfc61dafc15ea1af0f4bd62b10da95283e967a
Paweł Rudnik <pawelrudnik3@gmail.com>
2024-05-05 13:55:00 +0200

    Initial Commit

---
+++ FirebaseBootcampSwiftUI/FirebaseBootcampSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBootcampSwiftUIApp.swift
+//  FirebaseBootcampSwiftUI
+//
+//  Created by Paweł Rudnik on 05/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBootcampSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
