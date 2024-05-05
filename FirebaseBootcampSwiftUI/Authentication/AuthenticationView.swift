8d852eec7a65dfd5c7ed4739f11f9b1c6c1b3373
Paweł Rudnik <pawelrudnik3@gmail.com>
2024-05-05 14:57:32 +0200

    Add FirebaseAuth package. Create Sign In UI

---
+++ FirebaseBootcampSwiftUI/Authentication/AuthenticationView.swift
@@ -0,0 +1,37 @@
+//
+//  AuthenticationView.swift
+//  FirebaseBootcampSwiftUI
+//
+//  Created by Paweł Rudnik on 05/05/2024.
+//
+
+import SwiftUI
+
+struct AuthenticationView: View {
+    var body: some View {
+        VStack {
+            
+            NavigationLink {
+                SignInEmailView()
+            } label: {
+                Text("Sign In With Email")
+                    .font(.headline)
+                    .foregroundStyle(.white)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(.blue)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+            }
+            
+            Spacer()
+        }
+        .padding()
+        .navigationTitle("Sign In")
+    }
+}
+
+#Preview {
+    NavigationStack {
+        AuthenticationView()
+    }
+}
---
+++ FirebaseBootcampSwiftUI/Authentication/SignInEmailView.swift
@@ -0,0 +1,55 @@
+//
+//  SignInEmailView.swift
+//  FirebaseBootcampSwiftUI
+//
+//  Created by Paweł Rudnik on 05/05/2024.
+//
+
+import SwiftUI
+
+// viewModel for a SignInEmailView:
+final class SignInEmailViewModel: ObservableObject {
+    @Published var email = ""
+    @Published var password = ""
+}
+
+struct SignInEmailView: View {
+    
+    @StateObject private var viewModel = SignInEmailViewModel()
+    
+    var body: some View {
+        VStack {
+            TextField("Email...", text: $viewModel.email)
+                .padding()
+                .background(.gray.opacity(0.4))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            SecureField("Password...", text: $viewModel.password)
+                .padding()
+                .background(.gray.opacity(0.4))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            Button {
+                
+            } label: {
+                Text("Sign In")
+                    .font(.headline)
+                    .foregroundStyle(.white)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(.blue)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+            }
+            
+            Spacer()
+        }
+        .padding()
+        .navigationTitle("Sign In With Email")
+    }
+}
+
+#Preview {
+    NavigationStack {
+        SignInEmailView()
+    }
+}
