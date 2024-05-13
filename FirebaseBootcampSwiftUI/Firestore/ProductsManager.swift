190280a948b3c4c8e2c69f33f8349d154b2d70ec
Paweł Rudnik <pawelrudnik3@gmail.com>
2024-05-13 21:34:34 +0200

    CREATE QUERY EXTENSION TO FETCH MULTIPLE DOCUMENTS IN FIREBASE FIRESTORE

--- FirebaseBootcampSwiftUI/Firestore/ProductsManager.swift
+++ FirebaseBootcampSwiftUI/Firestore/ProductsManager.swift
@@ -25,18 +25,54 @@ final class ProductsManager {
     }
     
     
-    func getAllProducts() async throws -> [Product] {
-        let snapshot = try await productsCollection.getDocuments() // there is no func like getDocuments(as: ), try await userDocument(userId: userId).getDocument(as: DBUser.self), so we need to make a snapshot
-        
-        var products: [Product] = []
-        
-        // looping through the documents and convert them to the product individually
-        for document in snapshot.documents {
-            let product = try document.data(as: Product.self)
-            products.append(product)
-        }
-        
-        return products
+    func getProduct(productId: String) async throws -> Product {
+        try await productDocument(productId: productId).getDocument(as: Product.self)
     }
     
+    
+//    func getAllProducts() async throws -> [Product] {
+//        let snapshot = try await productsCollection.getDocuments() // there is no func like getDocuments(as: ), try await userDocument(userId: userId).getDocument(as: DBUser.self), so we need to make a snapshot
+//        
+//        var products: [Product] = []
+//        
+//        // looping through the documents and convert them to the product individually
+//        for document in snapshot.documents {
+//            let product = try document.data(as: Product.self)
+//            products.append(product)
+//        }
+//        
+//        return products
+//    }
+    
+    func getAllProducts() async throws -> [Product] {
+        try await productsCollection.getDocuments(as: Product.self)
+    }
+    
+}
+
+extension Query {
+    
+    func getDocuments<T>(as type: T.Type) async throws -> [T] where T : Decodable {
+        
+        let snapshot = try await self.getDocuments()
+        
+        return try snapshot.documents.map({ document in
+            try document.data(as: T.self)
+        })
+    }
+    
+    
+//    func getDocuments<T>(as type: T.Type) async throws -> [T] where T : Decodable {
+//        
+//        let snapshot = try await self.getDocuments()
+//        
+//        var products: [T] = []
+//        
+//        for document in snapshot.documents {
+//            let product = try document.data(as: T.self)
+//            products.append(product)
+//        }
+//        
+//        return products
+//    }
 }
